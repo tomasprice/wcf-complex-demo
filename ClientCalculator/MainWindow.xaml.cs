2dc18eb2cab03fd5deaa2cf250a750a5bc773d74
Tomasz Wodzynski <t910755@gmail.com>
2020-10-13 21:17:49 +0200

    Create client layout. Add reference to service

--- ClientCalculator/MainWindow.xaml.cs
+++ ClientCalculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClientCalculator.CalculatorServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,88 @@ namespace ClientCalculator
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btn_click_1(object sender, RoutedEventArgs e)
+        {
+            var calculator = new CalculatorServiceReference.CalculatorClient();
+
+            var result = calculator.Add(new ComplexNumber()
+            {
+                Real = 10,
+                Imaginary = 15
+            }, new ComplexNumber()
+            {
+                Real = 23,
+                Imaginary = 32
+            }, null);
+
+        }
+
+        private void btn_click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_dot(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_zero(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Operations
+        private void btn_click_result(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_clear(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_add(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_substract(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_multiply(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btn_click_divade(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
