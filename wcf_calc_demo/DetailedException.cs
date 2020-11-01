e2271d2be760e092d9adef45281ce7c8025a414f
Tomasz Wodzynski <t910755@gmail.com>
2020-11-01 21:19:14 +0100

    feat: create class for exception details

---
+++ wcf_calc_demo/DetailedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace wcf_calc_demo
+{
+    [DataContract]
+    public class DetailedException
+    {
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public string Details { get; set; }
+
+        [DataMember]
+        public DateTime Date { get; set; }
+    }
+}
