caf4bed77d2a49c175ba259d7e476c41800c65b4
Tomasz Wodzynski <t910755@gmail.com>
2020-11-01 21:20:03 +0100

    feat: add FaultContract attribute

--- wcf_calc_demo/ICalculator.cs
+++ wcf_calc_demo/ICalculator.cs
@@ -14,45 +14,53 @@ namespace wcf_calc_demo
     {
 
         [OperationContract]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Multiply(
             ComplexNumber complexNumber, 
             ComplexNumber complexNumber_2,
             string complex = null);
 
         [OperationContract(Name = "DoublesMultiply")]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Multiply(
             double real_1, double imaginary_1,
             double real_2, double imaginary_2);
 
         [OperationContract]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Substract(
             ComplexNumber complexNumber, 
             ComplexNumber complexNumber_2,
             string complex = null);
 
         [OperationContract(Name = "DoublesSubstract")]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Substract(
             double real_1, double imaginary_1,
             double real_2, double imaginary_2);
 
         [OperationContract]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Add(
             ComplexNumber complexNumber, 
             ComplexNumber complexNumber_2,
             string complex = null);
 
         [OperationContract(Name = "DoublesAdd")]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Add(
             double real_1, double imaginary_1,
             double real_2, double imaginary_2);
 
         [OperationContract]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Divade(
             ComplexNumber complexNumber, 
             ComplexNumber complexNumber_2,
             string complex = null);
 
         [OperationContract(Name = "DoublesDivade")]
+        [FaultContract(typeof(DetailedException))]
         ComplexNumber Divade(
             double real_1, double imaginary_1,
             double real_2, double imaginary_2);
